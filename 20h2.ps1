2a00b699b745c9a51aee414177b41358be8d2d0e
ofmn <munkieftw@gmail.com>
2022-03-02 14:35:42 +0100

    Include check to skip if already 20h2 Include OS version in field if PC has no Windows BT folder

--- 20h2.ps1
+++ 20h2.ps1
@@ -67,75 +67,75 @@ $Button1_Click = {
         }
         # It should test for both the OS version but also if the WindowsBT exist.
         # 20H2 = stop and just tell ITSS. No WindowsBT, also stop, because the rest of this code is dependendt on it being there
-        #If ($correctOSversion -ne $true) {
+        If ($correctOSversion -ne $true) {
             If (Test-path \\$hostname\c$\`$WINDOWS.~BT) {
-            # Get logs if exist
-            $script:sourcefolder = "\\$hostname\c$\`$WINDOWS.~BT\Sources\Panther"
-            $script:destinationfolder = "\\cher\m-drev\Trans\OLMN\20H2\$hostname"
-            If (!(Test-Path "$destinationfolder")) { New-Item -Path "\\cher\m-drev\Trans\OLMN\20H2" -Name "$hostname" -ItemType "directory" }
-            $log1_source = "$sourcefolder\setuperr.log"
-            $log1_dest = "$destinationfolder\setuperr.log"
-            $log2_source = "$sourcefolder\scanresult.xml"
-            $log2_dest = "$destinationfolder\scanresult.xml"
-            $log3_source = "$sourcefolder\setupact.log"
-            $log3_dest = "$destinationfolder\setupact.log"
-            If (Test-Path "$destinationfolder") {
-                try {
-                If (Test-Path $log1_source) { Copy-Item $log1_source -Destination $log1_dest }
-                If (Test-Path $log2_source) { Copy-Item $log2_source -Destination $log2_dest }
-                If (Test-Path $log3_source) { Copy-Item $log3_source -Destination $log3_dest }
-                }
-                catch [System.UnauthorizedAccessException] {
-                    Write-Host -ForegroundColor Red "Access Denied to $hostname";
-                    $ComputerName.Text = "Access Denied"
-                }
-                catch [System.Management.ManagementException] {
-                    Write-Host -ForegroundColor Red "Access Denied to $hostname";
-                    $ComputerName.Text = "Access Denied"
-                }
-                catch [System.Runtime.InteropServices.COMException] {
-                    Write-Host -ForegroundColor Red "The RPC Server is Unavailable on $hostname";
-                    clearformwindow
-                    $ComputerName.Text = "Computer offline"
-                }
+                # Get logs if exist
+                $script:sourcefolder = "\\$hostname\c$\`$WINDOWS.~BT\Sources\Panther"
+                $script:destinationfolder = "\\cher\m-drev\Trans\OLMN\20H2\$hostname"
+                If (!(Test-Path "$destinationfolder")) { New-Item -Path "\\cher\m-drev\Trans\OLMN\20H2" -Name "$hostname" -ItemType "directory" }
+                $log1_source = "$sourcefolder\setuperr.log"
+                $log1_dest = "$destinationfolder\setuperr.log"
+                $log2_source = "$sourcefolder\scanresult.xml"
+                $log2_dest = "$destinationfolder\scanresult.xml"
+                $log3_source = "$sourcefolder\setupact.log"
+                $log3_dest = "$destinationfolder\setupact.log"
+                If (Test-Path "$destinationfolder") {
+                    try {
+                        If (Test-Path $log1_source) { Copy-Item $log1_source -Destination $log1_dest }
+                        If (Test-Path $log2_source) { Copy-Item $log2_source -Destination $log2_dest }
+                        If (Test-Path $log3_source) { Copy-Item $log3_source -Destination $log3_dest }
+                    }
+                    catch [System.UnauthorizedAccessException] {
+                        Write-Host -ForegroundColor Red "Access Denied to $hostname";
+                        $ComputerName.Text = "Access Denied"
+                    }
+                    catch [System.Management.ManagementException] {
+                        Write-Host -ForegroundColor Red "Access Denied to $hostname";
+                        $ComputerName.Text = "Access Denied"
+                    }
+                    catch [System.Runtime.InteropServices.COMException] {
+                        Write-Host -ForegroundColor Red "The RPC Server is Unavailable on $hostname";
+                        clearformwindow
+                        $ComputerName.Text = "Computer offline"
+                    }
                 
-            }
-    
-            # Add log names of copied logs to array, and show the fullpath in Form window
-            $logfiles = @()
-            foreach ($entry in (Get-ChildItem $destinationfolder)) {
-                $fullnamelog = $entry.Fullname  
-                $logfiles += "`r`n$fullnamelog"
-            }
-
-            # Get disk space remaining
-            $win32_logicaldisk = Get-WmiObject Win32_LogicalDisk @wmipara -Filter "DeviceID='C:'"
-            $freeandtotal = ($win32_logicaldisk | Select-Object Size, FreeSpace)
-            $totaldisk = [Math]::Round($freeandtotal.Size / 1GB)
-            $diskspaceleft = [Math]::Round($freeandtotal.Freespace / 1GB)
-            If ($diskspaceleft -le 25) { $disktext = "Not enough diskspace! - Only $diskspaceleft GB left!" }
-            else { $disktext = "$diskspaceleft GB free / of $totaldisk GB" }
-
-            # If log file "scanresult.xml" exist search for "BlockMigration="True". As this means there are out of date drivers that needs updating.
-            # Count how many entries, and this will tell how many drivers need updating. Output number as filename to new file in folder.
-            If (Test-Path "$destinationfolder\Scanresult.xml") {
-                #-and (!(Test-path $folder\*date*))) 
-                $driverissue = select-string -Path "$destinationfolder\Scanresult.xml" -Pattern 'oem.{1,6}\.inf(?=" BlockMigration="True")' -AllMatches | ForEach-Object { $_.Matches } | ForEach-Object { $_.Value }
-                $drivercount = $driverissue.count
-                $script:driverarray = @()
-                foreach ($driver in $driverissue) {
-                (Get-childitem \\$hostname\c$\Windows\INF -Filter $driver).Fullname | ForEach-Object { Copy-Item -Path $_ -Destination "\\cher\m-drev\trans\olmn\20h2\$hostname" }
-                    $driverarray += $driver -replace ".*.inf", "`r`n\\$hostname\c$\Windows\INF\$driver"
                 }
-            }
+    
+                # Add log names of copied logs to array, and show the fullpath in Form window
+                $logfiles = @()
+                foreach ($entry in (Get-ChildItem $destinationfolder)) {
+                    $fullnamelog = $entry.Fullname  
+                    $logfiles += "`r`n$fullnamelog"
+                }
+
+                # Get disk space remaining
+                $win32_logicaldisk = Get-WmiObject Win32_LogicalDisk @wmipara -Filter "DeviceID='C:'"
+                $freeandtotal = ($win32_logicaldisk | Select-Object Size, FreeSpace)
+                $totaldisk = [Math]::Round($freeandtotal.Size / 1GB)
+                $diskspaceleft = [Math]::Round($freeandtotal.Freespace / 1GB)
+                If ($diskspaceleft -le 25) { $disktext = "Not enough diskspace! - Only $diskspaceleft GB left!" }
+                else { $disktext = "$diskspaceleft GB free / of $totaldisk GB" }
+
+                # If log file "scanresult.xml" exist search for "BlockMigration="True". As this means there are out of date drivers that needs updating.
+                # Count how many entries, and this will tell how many drivers need updating. Output number as filename to new file in folder.
+                If (Test-Path "$destinationfolder\Scanresult.xml") {
+                    #-and (!(Test-path $folder\*date*))) 
+                    $driverissue = select-string -Path "$destinationfolder\Scanresult.xml" -Pattern 'oem.{1,6}\.inf(?=" BlockMigration="True")' -AllMatches | ForEach-Object { $_.Matches } | ForEach-Object { $_.Value }
+                    $drivercount = $driverissue.count
+                    $script:driverarray = @()
+                    foreach ($driver in $driverissue) {
+                (Get-childitem \\$hostname\c$\Windows\INF -Filter $driver).Fullname | ForEach-Object { Copy-Item -Path $_ -Destination "\\cher\m-drev\trans\olmn\20h2\$hostname" }
+                        $driverarray += $driver -replace ".*.inf", "`r`n\\$hostname\c$\Windows\INF\$driver"
+                    }
+                }
     
 
-            If (Test-Path $destinationfolder\setuperr.log) {
-                $0x8007065e = select-string -Path "$destinationfolder\setuperr.log" -Pattern "Error reading if disable WU access is set, swallowing: \[0x8007065e]" -AllMatches | ForEach-Object { $_.Matches } | ForEach-Object { $_.Value }
-                If ($0x8007065e.count -ge 1) { $0x8007065e = $0x8007065e[0] }
-            }
-            $TextBox1.Text =
-            "Date:                      $date
+                If (Test-Path $destinationfolder\setuperr.log) {
+                    $0x8007065e = select-string -Path "$destinationfolder\setuperr.log" -Pattern "Error reading if disable WU access is set, swallowing: \[0x8007065e]" -AllMatches | ForEach-Object { $_.Matches } | ForEach-Object { $_.Value }
+                    If ($0x8007065e.count -ge 1) { $0x8007065e = $0x8007065e[0] }
+                }
+                $TextBox1.Text =
+                "Date:                      $date
 LastBoot:               $lastboot
 
 Hostname:               $hostname
@@ -148,31 +148,36 @@ Please attach all logs to ticket"
 
         
 
-            If (Test-Path $log1_dest) {
-                $Logsbutton.visible = $true
-                $Form1.Height = 554
-                $TABS.Visible = $true
-                $setuperrlogtextbox.Text = $0x8007065e
-            }
+                If (Test-Path $log1_dest) {
+                    $Logsbutton.visible = $true
+                    $Form1.Height = 554
+                    $TABS.Visible = $true
+                    $setuperrlogtextbox.Text = $0x8007065e
+                }
     
-            If (Test-Path $log2_dest) {
-                If ($drivercount -ge 1) {
-                    $scanresulttextbox.Text = "Driver issues found : $drivercount
+                If (Test-Path $log2_dest) {
+                    If ($drivercount -ge 1) {
+                        $scanresulttextbox.Text = "Driver issues found : $drivercount
 Please update with ThinInstaller.
             
 If this does not help resolve it, please open these files with notepad to check what driver it might be.
 $driverarray"
 
-                }
-                If ($drivercount -eq 0) { $scanresulttextbox.Text = "No outdated drivers." }
+                    }
+                    If ($drivercount -eq 0) { $scanresulttextbox.Text = "No outdated drivers." }
 
+                }
+                $TextBox1.Text | Out-File $destinationfolder\Textboxoutput.txt
             }
-            $TextBox1.Text | Out-File $destinationfolder\Textboxoutput.txt
         }
-        #If ($correctOSversion -eq $true) { $TextBox1.Text = "Computer successfully updated." }   
-        else {$TextBox1.Text = "Computer have not tried running the update yet.
+        If ($correctOSversion -eq $true) { $TextBox1.Text = "Computer successfully updated." }   
+        If (($correctOSversion -ne $true) -and (!(Test-Path \\$hostname\c$\`$WINDOWS.~BT))) {
+            $TextBox1.Text = "
+Computer have not tried running the update yet.
 This is usually due to load on the WSUS servers.
-Please keep the ticket and try again later or tomorrow"}
+Please keep the ticket and try again later or tomorrow
+OS Version : $osversion"
+        }
     }
     catch [System.UnauthorizedAccessException] {
         Write-Host -ForegroundColor Red "Access Denied to $hostname";
@@ -190,6 +195,7 @@ Please keep the ticket and try again later or tomorrow"}
     # Perform a new step in the progress bar to show progress Add 22
     $ProgressBar1.PerformStep()
 }
+
 
 $LogsButton_Click = {
     Invoke-Item -Path "$destinationfolder"
